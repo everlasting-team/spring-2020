bf658a5af65c2ffc1faae69e99886778b0e7a456
MayBlackwood <ksenia.bobtsova@gmail.com>
2020-04-16 12:25:02 +0300

    Get info about cards from api

--- src/components/LeftNav/index.jsx
+++ src/components/LeftNav/index.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import Input from "./../../common/Input";
+import axios from "axios";
 import "./index.scss";
 
 class LeftNav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue: '',
+      searchValue: "",
+      cards: null,
+      isLoading: false,
+      error: null,
     };
   }
 
   handleSearchInputChange = (e) => {
-    this.setState({searchValue: e.target.value});
+    this.setState({ searchValue: e.target.value });
   };
+
+  getCardsData = () => {
+    axios
+      .get("/tz20/list.json")
+      .then((res) => {
+        return res.data.data.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            shortInfo: item.shortInfo,
+            more: item.more,
+            type: "default",
+            deleted: false,
+            deletedDate: null,
+          };
+        });
+      })
+      .then((res) => {
+        this.setState({ cards: res, isLoading: false });
+      })
+      .catch((error) => this.setState({ error, isLoading: false }));
+  };
+
+  componentDidMount() {
+    this.getCardsData();
+  }
 
   render() {
     const { searchValue } = this.state;
